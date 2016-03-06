327b47cee3837ea283204e21c220279ae008f826
Elan Ruusamäe <glen@delfi.ee>
2016-03-06 19:22:43 +0000

    parse files in groups

--- slack-cvs-hook.py
+++ slack-cvs-hook.py
@@ -9,6 +9,7 @@
 
 import sys
 import subprocess
+from itertools import izip
 import slackweb
 
 # Get token from https://my.slack.com/services/new/incoming-webhook/
@@ -20,6 +21,11 @@ CHANNEL = "@glen"
 # Username to show in webook
 USERNAME = "CVS Commit"
 
+# http://stackoverflow.com/a/5389547
+def grouped(iterable, n):
+    "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
+    return izip(*[iter(iterable)]*n)
+
 from pprint import pprint
 
 user, module = sys.argv[1:3]
@@ -28,3 +34,6 @@ files = sys.argv[3:]
 pprint(user)
 pprint(module)
 pprint(files)
+
+for filename, oldrev, newrev in grouped(files, 3):
+   print "%s : %s->%s" % (filename, oldrev, newrev)
