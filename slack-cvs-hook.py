2c186fa3b355bd3c0110a553fa485e4f27216655
Elan Ruusamäe <glen@delfi.ee>
2016-03-06 19:39:14 +0000

    commit message parser

--- slack-cvs-hook.py
+++ slack-cvs-hook.py
@@ -26,11 +26,34 @@ def grouped(iterable, n):
     "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
     return izip(*[iter(iterable)]*n)
 
+"""
+Commit message is multiline with preformatted by CVS client.
+Read up STDIN up to: 'Log Message:\n',
+
+Example:
+['Update of /usr/local/cvs/test/slack\n',
+ 'In directory localhost:/home/glen/scm/cvs/test-CVSROOT/testdir/slack\n',
+ '\n',
+ 'Modified Files:\n',
+ '\tslack.txt slack2.txt \n',
+ 'Log Message:\n',
+ '- slack\n',
+ '\n']
+"""
+def get_commit_message():
+    lines = sys.stdin.readlines()
+
+    for i in range(len(lines)):
+        if lines[i] == "Log Message:\n":
+            break
+
+    return "".join(lines[i + 1:]).strip()
+
 from pprint import pprint
 
 user, module = sys.argv[1:3]
 files = sys.argv[3:]
-commit_msg = sys.stdin.readlines()
+commit_msg = get_commit_message()
 
 pprint(user)
 pprint(module)
