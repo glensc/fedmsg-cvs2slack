5e1133cd0827acfcced57f2fec6342b26b3d4a74
Elan Ruusamäe <glen@delfi.ee>
2017-07-21 13:42:39 +0300

    text version of fallback message

--- helper.py
+++ helper.py
@@ -11,12 +11,14 @@ def transform(msg):
 
     summary = "by *%s*: %s" % (msg['user'], msg['message'])
     text = ""
+    fallback = ""
     for f in msg['files']:
         u = f['urls']
         text += u"<%s|%s>: <%s|r%s> <%s|→> <%s|r%s>\n" % (u['log_url'], f['filename'], u['old_url'], f['old_rev'], u['diff_url'], u['new_url'], f['new_rev'])
+        fallback += u"%s r%s → r%s\n" % (f['filename'], f['old_rev'], f['new_rev'])
 
     attachments = [{
-        "fallback": text,
+        "fallback": fallback,
         "text": text,
         "color": "good",
     }]
