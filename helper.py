a46776c178ed079f60d50d13216949288c6c15c6
Elan Ruusamäe <glen@delfi.ee>
2017-07-21 14:15:27 +0300

    handle file adds/removals

--- helper.py
+++ helper.py
@@ -16,8 +16,18 @@ def transform(msg):
         # create dict of properties and urls
         d = dict(f, **f['urls'])
 
-        text += u"<%(log_url)s|%(filename)s>: <%(old_url)s|r%(old_rev)s> <%(diff_url)s|→> <%(new_url)s|r%(new_rev)s>\n" % d
-        fallback += u"%(filename)s r%(old_rev)s → r%(new_rev)s\n" % d
+        # diff
+        if f['old_rev'] and f['new_rev']:
+            text += u"<%(log_url)s|%(filename)s>: <%(old_url)s|r%(old_rev)s> <%(diff_url)s|→> <%(new_url)s|r%(new_rev)s>\n" % d
+            fallback += u"%(filename)s r%(old_rev)s → r%(new_rev)s\n" % d
+        # removed
+        elif f['old_rev']:
+            text += u"<%(old_url)s|%(filename)s>: <%(old_url)s|r%(old_rev)s> (removed)\n" % d
+            fallback += u"%(filename)s r%(old_rev)s (removed)\n" % d
+        # added
+        elif f['new_rev']:
+            text += u"<%(new_url)s|%(filename)s>: <%(new_url)s|r%(new_rev)s> (added)\n" % d
+            fallback += u"%(filename)s r%(new_rev)s (added)\n" % d
 
     attachments = [{
         "fallback": fallback,
